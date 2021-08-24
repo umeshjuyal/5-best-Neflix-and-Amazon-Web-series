e860ad290be71b414dc761413314dc691f0c2ab3
Umesh <umeshxz0000@gmail.com>
2021-08-24 11:38:39 +0530

    First commit index file added

---
+++ src/Sdata.jsx
@@ -0,0 +1,39 @@
+
+const Sdata =[
+  {
+    imgscr:"https://c4.wallpaperflare.com/wallpaper/210/493/206/tv-show-la-casa-de-papel-money-heist-tv-show-hd-wallpaper-preview.jpg ",
+     title:" A Netflix Original Series",
+      sname:"Money Heist",
+      links:" https://www.netflix.com/in/title/80192098",
+    
+},
+  {
+   imgscr:"https://wallpapercave.com/wp/wp6075995.jpg",
+    title:" A Netflix Original Series",
+      sname:"Family Man",
+      links:" https://www.primevideo.com/detail/amzn1.dv.gti.20bb765a-405b-250c-158b-c9979d16721a/ref_=dvm_pds_gen_in_as_s_gt_tfm2sited%7Cm_HPVHrkzEc_c386561644680",
+  },
+  {
+   imgscr:"https://static.wikia.nocookie.net/the-last-kingdom/images/e/ef/TLK_S2.png/revision/latest?cb=20200407224848.png",
+   title:" A Netflix Original Series",
+      sname:"The Last Kingdom",
+      links:" https://www.netflix.com/in/title/80074249",
+  
+     
+  },
+  {
+    imgscr:"https://moviesjack.in/wp-content/uploads/2021/01/31.jpg ",
+     title:" A Netflix Original Series",
+      sname:"Dark",
+      links:" https://www.netflix.com/in/title/80100172",
+      
+  },
+  {
+    imgscr:"  https://images-na.ssl-images-amazon.com/images/I/91XGV3uGpFL.jpg",
+     title:" A Netflix Original Series",
+      sname:"The Umbrella Academy",
+      links:" https://www.netflix.com/in/title/80186863",
+      
+  },
+  ];
+  export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// function Card(props){
+//   console.log(props);
+// return( 
+// <>
+//   <div className="cards">
+//   <div className="card">
+//   <img src ={ props.imgsrc}alt ="my Pic" className="card_img"/>
+//   <div className="card_info">
+//       <span className="card_category">{props.title} </span>
+//       <h3 className="card_title">{props.sname}</h3>
+//       <a href={props.link} target ="blank">
+//           <button>Watch Now</button>
+//       </a>
+//   </div>
+
+//   </div>
+//   </div>
+//   </>
+
+// );
+// }
+
+//  For 5 Netflix and Amazon Prime series
+
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./Cards"; 
+import "./index.css";
+import Sdata from './Sdata';
+
+// console.log(Sdata[0])
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <>
+  <h1 className="heading_style">List of top 5 Neflix and Amazon Prime Series</h1>
+  <Card 
+  imgsrc={Sdata[0].imgscr}
+  title={Sdata[0].title}
+  sname={Sdata[0].sname}
+  link={Sdata[0].links}
+  />
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <Card
+  imgsrc={Sdata[1].imgscr}
+  title={Sdata[1].title}
+  sname={Sdata[1].sname}
+  link={Sdata[1].links}
+/>
+
+  <Card
+  imgsrc={Sdata[2].imgscr}
+  title={Sdata[2].title}
+  sname={Sdata[2].sname}
+  link={Sdata[2].links}
+/>
+<Card 
+   imgsrc={Sdata[3].imgscr}
+  title={Sdata[3].title}
+  sname={Sdata[3].sname}
+  link={Sdata[3].links}
+  />
+  <Card 
+  imgsrc={Sdata[4].imgscr}
+  title={Sdata[4].title}
+  sname={Sdata[4].sname}
+  link={Sdata[4].links}
+  />
+
+  </>,
+document.getElementById('root')
+);
